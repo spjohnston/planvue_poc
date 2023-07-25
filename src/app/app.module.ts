7cb1a37dc050038c8a91c162dcc5ba5c581c24e1
steve-overjet <steve@overjet.ai>
2023-07-24 20:54:02 -0400

    auth0: register + configure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AuthModule.forRoot({
+      domain: 'dev-svfxidsxvj5w0nvq.us.auth0.com',
+      clientId: 'Q4uuPVkuU8ANsGSou0j0UaBJvWBY7QaF',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
